d1a9b5c14829e7b111d619f6eed31e501673dc14
Swann Henaux <108821353+H8sh@users.noreply.github.com>
2023-05-28 18:47:07 +0200

    config modif

--- tina/config.ts
+++ tina/config.ts
@@ -5,7 +5,7 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
+  clientId: "0a1cd584-7257-4a7d-8b00-ebe3a2181775", // Get this from tina.io
   token: null, // Get this from tina.io
 
   build: {
